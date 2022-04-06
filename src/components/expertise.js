16e56b3cbb1228f15c776e81a0b78f87755b6191
patrick lehmann <ptrcklehmann@gmail.com>
2022-04-06 16:22:09 +0200

    fix expertise grid

--- src/components/expertise.js
+++ src/components/expertise.js
@@ -3,28 +3,26 @@ import React from 'react'
 import Container from './container'
 import * as styles from './expertise.module.css'
 
-const Expertise = ({ content }) => {
-  if (!content) return null
-  if (!Array.isArray(content)) return null
+const Expertise=({content}) => {
+    if(!content) return null
+    if(!Array.isArray(content)) return null
 
-  return (
-    <Container>
-      <ul className={styles.expertiseList}>
-        {content.map((text) => {
-          return (
-            <li key={text.expertiseTitle}>
-              <h1>{text.expertiseTitle}</h1>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: text.expertiseText.childMarkdownRemark.html,
-                }}
-              />
-            </li>
-          )
-        })}
-      </ul>
-    </Container>
-  )
+    return (
+        <Container>
+            <ul className={styles.expertiseList}>
+                {content.map((text) => {
+                    return (
+                        <li key={text.expertiseTitle}>
+                            <h1>{text.expertiseTitle}</h1>
+                            <div
+                            dangerouslySetInnerHTML={{__html:text.expertiseText?.childMarkdownRemark?.html}}
+                            />
+                        </li>
+                    )
+                })}
+            </ul>
+        </Container>
+    )
 }
 
 export default Expertise
